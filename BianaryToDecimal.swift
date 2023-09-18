8b3762253ad44c5c403d49fb84c1401e2dd53cc6
Rahul Basu <rbasu5027@stu.d214.org>
2023-09-18 11:18:12 -0500

    t

--- BianaryToDecimal.swift
+++ BianaryToDecimal.swift
@@ -5,4 +5,44 @@
 //  Created by Subham Pathak on 9/18/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct BinaryToDecimal: View {
+    @State private var binaryInput = ""
+    @State private var decimalOutput = ""
+    
+    var body: some View {
+        VStack {
+            Text("Binary to Decimal Converter")
+                .font(.largeTitle)
+                .padding()
+            
+            TextField("Enter Binary Number", text: $binaryInput)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                convertBinaryToDecimal()
+            }) {
+                Text("Convert")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Text("Decimal: \(decimalOutput)")
+                .font(.headline)
+                .padding()
+        }
+        .padding()
+    }
+    
+    func convertBinaryToDecimal() {
+        guard let binaryNumber = Int(binaryInput, radix: 2) else {
+            decimalOutput = "Invalid binary input!"
+            return
+        }
+        decimalOutput = String(binaryNumber)
+    }
+}
--- BinaryToHexadecimal.swift
+++ BinaryToHexadecimal.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct BinaryToHexadecimal: View {
+    @State private var binaryInput = ""
+    @State private var hexadecimalOutput = ""
+    
+    var body: some View {
+        VStack {
+            Text("Binary to Hexadecimal Converter")
+                .font(.largeTitle)
+                .padding()
+            
+            TextField("Enter Binary Number", text: $binaryInput)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                convertBinaryToHex()
+            }) {
+                Text("Convert")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Text("Hexadecimal: \(hexadecimalOutput)")
+                .font(.headline)
+                .padding()
+        }
+        .padding()
+    }
+    
+    func convertBinaryToHex() {
+        guard let binaryNumber = Int(binaryInput, radix: 2) else {
+            hexadecimalOutput = "Invalid binary input!"
+            return
+        }
+        
+        hexadecimalOutput = String(binaryNumber, radix: 16).uppercased()
+    }
+}
